805ba375f9194700d9d47eb24a67326c11229214
Deokgyun an <97350241+deokgyun@users.noreply.github.com>
2023-06-30 10:11:50 +0900

    Remove: JPA -> MYBATIS 변경으로 사용파일 삭제

--- src/main/java/com/project/MelodyMateBackend/domain/Song/SongRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.MelodyMateBackend.domain.Song;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-
-
-}
